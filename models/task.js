4e7c28517fefad4833754d7d7891cb68332cb25f
Ane <42421075+anekekarina99@users.noreply.github.com>
2023-09-07 06:43:07 +0000

    all

---
+++ models/task.js
@@ -0,0 +1,21 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Task extends Model {
+      static associate(models) {
+            // Define associations (if any) with other models here
+                }
+                  }
+                    Task.init(
+                        {
+                              title: DataTypes.STRING,
+                                    description: DataTypes.TEXT,
+                                        },
+                                            {
+                                                  sequelize,
+                                                        modelName: 'Task',
+                                                            }
+                                                              );
+                                                                return Task;
+                                                                };
+                                                                
